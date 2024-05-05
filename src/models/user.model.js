695b35df145fc4912a789457c40be9f550dac79c
Abhishek Kumar Jha <114056135+Abhishekjha2021@users.noreply.github.com>
2024-05-05 16:49:41 +0530

    new backend setups

---
+++ src/models/user.model.js
@@ -0,0 +1,105 @@
+import mongoose, {Schema} from "mongoose"
+import bcrypt from "bcrypt"
+import jsonwebtoken from "jsonwebtoken"
+
+const userSchema = new Schema({
+
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowerCase:true,
+        trim:true,
+        index:true
+    },
+
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowerCase:true,
+        trim:true,
+    },
+
+    fullName:{
+        type:String,
+        required:true,
+        trim:true,
+        index:true
+    },
+
+    avatar:{
+        type:String,        //cloudinary url---------
+        required:true
+    },
+
+    coverImage:{
+        type:String         //cloudinary url--------
+    },
+
+    watchHistory:[          //this field is an array 
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Video"
+        }
+    ],
+
+    password:{
+        type:String,
+        required:[true,"Password is required"]
+    },
+
+    refreshToken:{
+        type:true
+    }
+
+},{timestamps:true,versionKey:false})
+
+//using an encryption method by using "pre" hook-----------
+userSchema.pre("save",async function (next){
+
+    if(this.isModified("password")){        
+        this.password=bcrypt.hash(this.password,10)
+        next()
+    }
+    else{
+        return next();
+    }
+})
+ 
+userSchema.methods.isPasswordCorrect=async function (password){
+    return await bcrypt.compare(password,this.password)         //it returns value in true and false----------
+}
+
+
+
+//for generating access token and refresh token----------
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+export const  User= mongoose.model("User",userSchema)
---
+++ src/models/video.model.js
@@ -0,0 +1,51 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "npm i mongoose-aggregate-paginate-v2";
+
+const videoSchema=new Schema({
+    videoFile:{
+        type:String,        //cloudinary url-----------
+        required:true
+    },
+
+    thumbNail:{
+        type:String,        //cloudinary url-----------
+        required:true
+    },
+
+    title:{
+        type:String,        
+        required:true
+    },
+
+    description:{
+        type:String,        
+        required:true
+    },
+
+    duration:{
+        type:Number,        //cloudinary url---------
+        required:true
+    },
+
+    views:{
+        type:Number,
+        default:0
+    },
+
+    isPublished:{
+        type:Boolean,        //cloudinary url-----------
+        default:true
+    },
+
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
+
+
+
+},{timestamps:true,versionKey:false})
+
+videoSchema.plugin(mongooseAggregatePaginate)  //plugin is a hook here ....read about mongoDB hooks------
+
+export const Video=mongoose.model("Video",videoSchema)
